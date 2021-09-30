d565dc66a7536c52c009de373934c6f38f81ec0f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-09-30 20:48:36 +0300

    Rubocopped the feed.rb file

--- feed.rb
+++ feed.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# coding: utf-8
+
 # Main file of this project
 # Written by Henri Hyyryläinen
 require 'logger'
@@ -17,40 +17,34 @@ require_relative 'database'
 # Suppress tons of messages
 LogUtils::Logger[FeedParser::Parser].level = Logger::INFO
 
-puts "Verifying database"
+puts 'Verifying database'
 
 verifyDatabasesAndTablesExist
 
-puts "Starting RSS feed to discord webhook bot"
+puts 'Starting RSS feed to discord webhook bot'
 
 $runFeeds = true
 
-Signal.trap("INT") {
-  
+Signal.trap('INT') do
   $runFeeds = false
   $feedThread.wakeup
-}
+end
 
-$feedThread = Thread.new {
-
+$feedThread = Thread.new do
   while $runFeeds
     items = []
-    
-    puts "Fetching feeds..."
 
-    Feeds.each{|feedInfo|
+    puts 'Fetching feeds...'
 
+    Feeds.each do |feedInfo|
       feedParser = FeedParser::Parser.parse(open(feedInfo).read)
       items += feedParser.items
-    }
-
-    # Skip handling the items if we should quit
-    if !$runFeeds
-      break
     end
 
-    items.each{|item|
+    # Skip handling the items if we should quit
+    break unless $runFeeds
 
+    items.each do |item|
       # Check is it already sent
       puts "Handling item '#{item.id}'"
 
@@ -59,51 +53,46 @@ $feedThread = Thread.new {
         next
       end
 
-      formatted = "New post by #{item.author.to_s.split(' ')[0]} in topic "+
-                  "#{item.title}\n#{item.url}"      
-      
-      puts "Sending item to discord: " + formatted
+      formatted = "New post by #{item.author.to_s.split(' ')[0]} in topic " +
+                  "#{item.title}\n#{item.url}"
+
+      puts 'Sending item to discord: ' + formatted
 
       # This is a hacky way to follow the rate limiting
       builder = Discordrb::Webhooks::Builder.new
 
       builder.content = formatted
-      if CustomUserName
-        builder.username = CustomUserName
-      end
+      builder.username = CustomUserName if CustomUserName
 
       # Send it
       # could append ?wait=true to the url
-      response = Discordrb::API::request(:webhook, WebhookMajorParameter, "post", WebhookURL,
-                                         builder.to_json_hash.to_json, content_type: :json)
+      response = Discordrb::API.request(:webhook, WebhookMajorParameter, 'post', WebhookURL,
+                                        builder.to_json_hash.to_json, content_type: :json)
 
       puts "Got response: #{response.code}"
 
       if response.code != 200 && response.code != 204
-        
+
         # Failed
-        puts "Request failed. Not marking as done"
+        puts 'Request failed. Not marking as done'
 
       else
         # Mark item as done
-        puts "Marking item as done"
+        puts 'Marking item as done'
         setPostAsSent item.id, item.url
-        
-      end
-    }
 
-    puts "Done handling feeds. Waiting for next run"
+      end
+    end
+
+    puts 'Done handling feeds. Waiting for next run'
 
     # And allow quitting before waiting
-    if !$runFeeds
-      break
-    end
-    
+    break unless $runFeeds
+
     sleep UpdateEveryNSeconds
   end
 
-  puts "Ended feed parsing thread"
-}
+  puts 'Ended feed parsing thread'
+end
 
 $feedThread.join
-
